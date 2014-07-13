4880fb72c0f0659807f19ca8f30b4cd49eda2023
David James <david@bluemontlabs.com>
2014-07-13 10:05:28 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject destruct "0.1.0-SNAPSHOT"
-  :description "Try out destructuring"
-  :url "TODO"
-  :license {:name "TODO" :url "TODO"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :description "Clojure destructuring examples"
+  :url "https://github.com/xpe/clj-destruct"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
